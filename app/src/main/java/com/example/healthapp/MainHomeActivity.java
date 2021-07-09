4aeee8b99495db1bbc67eca4e76ba2d1eb427033
Akhil Padmanaban_ <akhilpadmanaban123@gmail.com>
2021-07-10 02:48:09 +0530

    Merged exercise page

--- app/src/main/java/com/example/healthapp/MainHomeActivity.java
+++ app/src/main/java/com/example/healthapp/MainHomeActivity.java
@@ -2,13 +2,29 @@ package com.example.healthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainHomeActivity extends AppCompatActivity {
+public class MainHomeActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView exercise;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
+
+   exercise=findViewById(R.id.exercise1);
+   exercise.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.exercise1:
+                startActivity(new Intent(MainHomeActivity.this,MainExercisePage.class));
+                break;
+        }
     }
 }
